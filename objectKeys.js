bb41aa52dc8d3e1afb8572c9a866f71c87da1cfd
Hector Norza <hnorza@Hectors-MBP.officenomads.com>
2018-06-07 12:14:10 -0700

    added ObjectKeys method example

---
+++ objectKeys.js
@@ -0,0 +1,16 @@
+'use strict';
+
+//This method returns an array of the keys of an object
+//Example: 
+
+const iceCreamColors = {
+  chocolate: 'brown',
+  vanilla: 'white',
+  strawberry: 'red'
+}
+const types = Object.keys(iceCreamColors);
+
+console.log(iceCreamColors);
+
+//result should be: {chocolate: "brown", vanilla: "white", strawberry: "red"}
+
